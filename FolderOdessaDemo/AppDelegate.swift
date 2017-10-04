f33408def3bab0f64a5766dded435c23d941b59d
SerhiiM <msm72@mail.ru>
2017-10-04 18:44:51 +0300

    Case 1 is ready

--- FolderOdessaDemo/AppDelegate.swift
+++ FolderOdessaDemo/AppDelegate.swift
@@ -10,17 +10,13 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    // MARK: - Class Functions
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
