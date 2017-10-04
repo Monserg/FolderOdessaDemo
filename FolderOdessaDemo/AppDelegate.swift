8bb96a033feca4d08c8be27a2ea46986b5b0f91c
SerhiiM <msm72@mail.ru>
2017-10-04 18:12:40 +0300

    first commit

---
+++ FolderOdessaDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FolderOdessaDemo
+//
+//  Created by msm72 on 04.10.2017.
+//  Copyright © 2017 RemoteJob. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FolderOdessaDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FolderOdessaDemo
+//
+//  Created by msm72 on 04.10.2017.
+//  Copyright © 2017 RemoteJob. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
